7e45ca4867ea04c97b6ea99adcfd6aa0c50defe7
Shibalik Dhara <78715726+Rivu78@users.noreply.github.com>
2024-01-23 21:20:51 +0530

    Add files via upload

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Income Tax Calculator</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="A straightforward Indian Income Tax calculator for India" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
